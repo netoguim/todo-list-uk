6e8bda3e081b097129d02edea7949d62e3dad1b7
Neto Guimarães <neto--guimaraes1@hotmail.com>
2021-12-27 09:37:37 -0300

    Criaçao das rotas e metodos

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -6,5 +6,30 @@ use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    //
+    
+    public function createTodo() {
+
+
+    }
+
+    public function readAllTodos() {
+
+        
+    }
+
+    public function readTodo() {
+
+        
+    }
+
+    public function updateTodo() {
+
+        
+    }
+
+    public function deleteTodo() {
+
+        
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,25 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\ApiController;
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+
+Route::get('/ping', function(){
+    return ['pong' => true];
 });
+
+
+//CRUD do todo 
+
+//POST /todo = INSERIR TAREFA
+//GET /todos = LER TODAS AS TAREFAS DO SISTEMA
+//GET /todo/2 = LER UMA TAREFA ESPECÍFICA DO SISTEMA 
+//PUT /todo/2 = ATUALIZAR UMA TAREFA NO SISTEMA
+//DELETE /todo/2 = DELETAR UMA TAREFA NO SISTEMA
+
+Route::post('/todo', [ApiController::class, 'createTodo']);
+Route::get('/todos', [ApiController::class, 'readAllTodos']);
+Route::get('/todo/{id}', [ApiController::class, 'readTodo']);
+Route::put('/todo/{id}', [ApiController::class, 'updateTodo']);
+Route::post('/todo/{id}', [ApiController::class, 'deleteTodo']);
